f31a99bd4218ad770a59e1be5aa0ac9166ed8419
VoidPointerLover69 <82274315+VoidPointerLover69@users.noreply.github.com>
2023-07-30 13:11:32 +0200

    Create stdasm.h

---
+++ stdasm.h
@@ -0,0 +1,11 @@
+//made by VoidPointerLover69
+//for use of the assembly project in C
+#ifndef STDASM_C
+#define STDASM_C
+
+extern void writestrl(const char* str, unsigned long long size);
+extern int stoi(const char* str);
+extern void getstrl(char* str, unsigned long long size);
+extern unsigned long long strlength(const char* str);
+
+#endif
